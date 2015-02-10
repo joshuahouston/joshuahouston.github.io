c8f0e5778b572e3662c0ae4cd4586baaf664972a
joshuahouston <joshuahouston@gmail.com>
2015-02-09 20:32:16 -0900

    Test kind of

---
+++ maps/harrypotter/icons-ext.js
@@ -0,0 +1,81 @@
+var myIcon = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/bolt.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/boltx2.png',
+	iconSize: [18, 46],
+	iconAnchor: [5, 44],
+	popupAnchor: [0, -40],
+	shadowUrl: 'http://jfact0ry.com/maps/harrypotter/shadow.png',
+	shadowRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/shadowx2.png',
+	shadowSize: [27, 16],
+	shadowAnchor: [13, 6]
+	});
+	
+var barty = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/barty.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/bartyx2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+});
+	
+var connie = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/connie.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/conniex2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+});
+	
+var jasmine = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/jasmine.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/jasminex2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+});
+	
+var ellen = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/ellen.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/ellenx2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+});
+
+var joshua = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/joshua.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/joshuax2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+	});
+	
+var hagrid = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/hagrid.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/hagridx2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+	});
+	
+var fudge = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/fudge.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/fudgex2.png',
+	iconSize: [160, 50],
+	iconAnchor: [79, 24]
+	});
+	
+var harry = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/harry.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/harryx2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+	});
+
+var hermione = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/hermione.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/hermionex2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+	});
+
+var snape = L.icon({
+	iconUrl: 'http://jfact0ry.com/maps/harrypotter/snape.png',
+	iconRetinaUrl: 'http://jfact0ry.com/maps/harrypotter/snapex2.png',
+	iconSize: [150, 56],
+	iconAnchor: [75, 29]
+	});
